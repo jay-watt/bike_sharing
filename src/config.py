e1a7222e6a1004c898e557521adc5c7540202cb2
Jay Watt <jaywatt8000@gmail.com>
2024-08-12 16:33:52 +1200

    Improve readability by adding headings and comments where necessary

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
 RAW_TRAIN_PATH = '../data/raw/train.csv'
 RAW_TEST_PATH = '../data/raw/test.csv'
 CATEGORICAL_FEATURES = ["season", "holiday", "workingday", "weather"]
+TARGET_VARIABLES = ['casual', 'registered', 'count']
--- src/data_processing.py
+++ src/data_processing.py
@@ -3,28 +3,12 @@ from config import RAW_TRAIN_PATH, RAW_TEST_PATH, CATEGORICAL_FEATURES
 
 
 def load_data(filepath):
-    """
-    Load data from a CSV file into a pandas DataFrame.
-
-    Parameters:
-    - filepath (str): The path to the CSV file.
-
-    Returns:
-    - DataFrame: A pandas DataFrame containing the data from the CSV file.
-    """
-    # Load the CSV file into a DataFrame
     df = pd.read_csv(filepath, parse_dates=['datetime'])
 
     return df
 
 
 def load_train_and_test_data():
-    """
-    Load training and testing data from CSV files.
-
-    Returns:
-    - tuple: A tuple containing the training and testing DataFrames.
-    """
     train_data = load_data(RAW_TRAIN_PATH)
     test_data = load_data(RAW_TEST_PATH)
 
@@ -38,8 +22,16 @@ def perform_categorical_conversion(data):
     return data
 
 
+def convert_datetime_to_timestamp(data):
+    data['datetime'] = data['datetime'].astype(
+        'int64') // (10**9 * 3600)  # Convert to hours
+
+    return data
+
+
 def preprocess_data(data):
     perform_categorical_conversion(data)
+    convert_datetime_to_timestamp(data)
 
 
 def preprocess_train_and_test_data(train_data, test_data):
